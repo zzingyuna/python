d24e3ca6f8894299d925901af6883fa1e57646cf
zzingyuna <kya7637@hanmail.net>
2021-08-05 22:53:23 +0900

    .

--- test_4.py
+++ test_4.py
@@ -1,3 +1,5 @@
+import re
+
 class SCDObj:
     title = ''
     docid = ''
@@ -9,22 +11,39 @@ tag = ['DOCID', 'TITLE'];
 listall = [];
 
 f = open("C:\\Users\\yuna\\Desktop\\test\\test1.SCD", 'r', encoding='UTF8')
-
+p = re.compile("\<[A-Z]*\>")
 lines = f.readlines()
-e = SCDObj();
+e = SCDObj()
+lastfield = ''
 for line in lines:
     if line.find('<DOCID>') != -1 :
         if len(e.docid) > 0 :
             listall.append(e)
+            e = SCDObj()
         
         e.docid = line.replace('<DOCID>', '')
+        lastfield = 'DOCID'
     elif line.find('<TITLE>') != -1 :
         e.title = line.replace('<TITLE>', '')
-    #print(line)
+        lastfield = 'TITLE'
+    elif line.find('<CONTENT>') != -1 :
+        e.content = line.replace('<CONTENT>', '')
+        lastfield = 'CONTENT'
+    elif line.find('<WRITER>') != -1 :
+        e.writer = line.replace('<WRITER>', '')
+        lastfield = 'WRITER'
+    elif p.match(line) == None:
+        if lastfield == "CONTENT":
+            e.content = e.content + line
+    # print(p.match(line))
+
+if len(e.docid) > 0 :
+    listall.append(e)
 
 f.close()
 
 
 for item in listall:
-    print(item.title)
+    print(item.content)
+    print('______________')
     
