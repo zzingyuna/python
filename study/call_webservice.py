26f06f1662a7ece99fedc28737535617c898258b
zzingyuna <kya7637@hanmail.net>
2022-08-26 13:59:39 +0900

    .

---
+++ study/call_webservice.py
@@ -0,0 +1,29 @@
+
+from suds.client import Client
+from suds.xsd.doctor import Import, ImportDoctor
+import logging
+import base64
+
+######################################
+# 파이썬에서 웹서비스 호출(.asmx)
+######################################
+
+
+url = 'http://도메인/DeadLinkSolutionGW.asmx?WSDL'
+imp = Import('http://www.w3.org/2001/XMLSchema')    # the schema to import.
+imp.filter.add('http://도메인/DeadLinkSolutionGW.asmx')  # the schema to import into.
+d = ImportDoctor(imp)
+client = Client(url, doctor=d)
+
+tokenKey = "토큰키값"
+
+res = client.service.GetLoadPattern(tokenKey)
+dataset = res[1]
+datatable = dataset[0]
+i = 0
+
+for dt in datatable[0]:
+	deadLinkPattern = dt['Dead_Link_Pattern1'].encode('euc-kr')
+	sourceSiteNm = dt['SourceSiteNm']
+	print(sourceSiteNm)
+
